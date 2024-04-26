c834157ab1ab778b04ac0f0d9daf53d7f977e33d
PALAPARTHI LOKESH <155537382+PALAPARTHILOKESH@users.noreply.github.com>
2024-04-27 04:50:14 +0530

    Update Login.js

--- Bidding/src/Components/Login.js
+++ Bidding/src/Components/Login.js
@@ -31,7 +31,7 @@ const Login = () => {
     }
 
     try {
-      const response = await axios.post('http://localhost:8081/check', { username, password });
+      const response = await axios.post('hhttp://obs-backend.vercel.app/check', { username, password });
       const responseData = response.data;
 
       if (responseData === 'Login successful' && username !== 'admin') {
